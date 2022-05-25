4f20f096ff07c3f9aa47c60ed4f4173b6198d618
Winnie <wrm956@gmail.com>
2022-05-24 23:28:27 -0400

    commit tree 3299e7f536c79e0d5b0a1ebf9453232dc7022d68

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const toRemove = document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+// <h1></h1>
+newHeader.id = 'victory'
+// <h1 id="victory"></h1>
+newHeader.textContent = 'Winnie is the champion'
+document.body.append(newHeader)
